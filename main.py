f679a14115333d366d6f403ac5ba62ec148c36c8
Tamer <tamer@mirror.com>
2026-04-14 06:26:04 +0200

    Fix Manifest package naming and add permissions

--- main.py
+++
@@ -1,9 +0,0 @@
-from kivy.app import App
-from kivy.uix.label import Label
-
-class MirrorApp(App):
-    def build(self):
-        return Label(text="Hello Tamer - Mirror is Alive")
-
-if __name__ == "__main__":
-    MirrorApp().run()
