4c1491627e860ff004f7af6d9037c730c8229867
Tamer <tamer@mirror.com>
2026-04-18 14:02:49 +0200

    Fix: Re-structure package directory to standard tree format

--- app/src/main/java/org/tetocollctionway/mirror/MainActivity.kt
+++ app/src/main/java/org/tetocollctionway/mirror/MainActivity.kt
@@ -4,18 +4,12 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            Greeting("Mirror Scorpion 2026 Ready")
+            Text(text = "Mirror Scorpion 2026: AI Ready")
         }
     }
-}
-
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
 }
