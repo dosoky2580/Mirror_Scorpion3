154f7faf5dac4e02557b37887a45f7b1836721f1
Tamer <tamer@mirror.com>
2026-04-18 18:28:36 +0200

    Emergency Rollback: Reverting to exact configuration of successful build #44

--- app/src/main/java/org/tetocollctionway/mirror/MainActivity.kt
+++ app/src/main/java/org/tetocollctionway/mirror/MainActivity.kt
@@ -1,23 +1,11 @@
 package org.tetocollctionway.mirror
 
-import android.content.Intent
 import android.os.Bundle
-import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        findViewById<Button>(R.id.btnTextTranslation).setOnClickListener {
-            startActivity(Intent(this, TranslationActivity::class.java))
-        }
-        findViewById<Button>(R.id.btnConversation).setOnClickListener {
-            startActivity(Intent(this, ConversationActivity::class.java))
-        }
-        findViewById<Button>(R.id.btnSettings).setOnClickListener {
-            startActivity(Intent(this, SettingsActivity::class.java))
-        }
     }
 }
