231e1c6f2ab9973a3b2421abb5bbd02ef91dfcbf
Tamer <tamer@mirror.com>
2026-04-19 20:03:20 +0200

    Feature: Integrated iText7 for PDF processing with parallel configuration

---
+++ app/src/main/java/org/tetocollctionway/mirror/DocumentTranslateActivity.kt
@@ -0,0 +1,11 @@
+package org.tetocollctionway.mirror
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class DocumentTranslateActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        // سننشئ ملف الـ Layout في الخطوة القادمة أو نستخدم واجهة بسيطة
+    }
+}
